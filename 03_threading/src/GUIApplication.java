774dc18e16ad670894790f5acb37710da3824adf
vla5924 <vla5924@gmail.com>
2021-03-21 15:56:11 +0300

    Adding multiple clock windows

--- 03_threading/src/GUIApplication.java
+++ 03_threading/src/GUIApplication.java
@@ -1,11 +1,11 @@
-import Forms.ClockWindow;
+import Forms.MainWindow;
 
 import javax.swing.*;
 
 public class GUIApplication {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Clock");
-        frame.setContentPane(new ClockWindow().getPanel());
+        JFrame frame = new JFrame("Clock manager");
+        frame.setContentPane(new MainWindow().getPanel());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
